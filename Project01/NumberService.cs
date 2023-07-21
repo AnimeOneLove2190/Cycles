e0932964933ec731306b6d05ce70ac73118210fb
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-07-22 03:03:26 +0500

    Добавьте файлы проекта.

---
+++ Project01/NumberService.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project01
+{
+    class NumberService
+    {
+        //Quest01
+        public int AddUpTheNumbers(int inputNum)
+        {
+            int result = 0;
+            int numAdd = 0;
+            for (int i = 0; i < inputNum; i++)
+            {
+                numAdd++;
+                result = result + numAdd;
+            }
+            return result;
+        }
+        //Quest02
+        public void WriteEvenNumbers(int inputNum)
+        {
+            for (int i = 0; i <= inputNum; i++)
+            {
+                int conditionalEven = i % 2;
+                if (conditionalEven == 1)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+        //Quest03
+        public bool CheckEasyNumbers(int inputNum)
+        {
+            if (inputNum == 2)
+            {
+                return true;
+            }
+            int division = 0;
+            int numberAdd = 2;
+            for (int i = 2; i < inputNum; i++)
+            {
+
+                division = inputNum % numberAdd;
+                numberAdd++;
+                if (division == 0)
+                {
+                    break;
+                }
+            }
+            if (!(division == 0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //Quest04
+        public int AddReadNumbers(int inputNum)
+        {
+            Random random = new Random();
+            int result = 0;
+            for (int i = 0; i < inputNum; i++)
+            {
+
+                int numOne = random.Next(int.MinValue, int.MaxValue);
+                result = result + numOne;
+            }
+            return result;
+        }
+        //Quest05
+        public double AverageOfNumbers(int inputNum)
+        {
+            Random random = new Random();
+            int added = 0;
+            double average = 0;
+            int numberOfNumbers = 0;
+            for (int i = 0; i < inputNum; i++)
+            {
+
+                int numOne = random.Next(int.MinValue, int.MaxValue);
+                numberOfNumbers++;
+                added = added + numOne;
+                average = (double)added / (double)numberOfNumbers;
+            }
+            return average;
+        }
+        //Quest06
+        public int FindMinNumbers(int inputNum)
+        {
+            Random random = new Random();
+            int numMin = int.MaxValue;
+            int result = 0;
+            for (int i = 0; i < inputNum; i++)
+            {
+                int numOne = random.Next(int.MinValue, int.MaxValue);
+                if (numOne <= numMin)
+                {
+                    int numMax = numMin;
+                    numMin = numOne;
+                    numOne = numMax;
+                    result = numMin;
+                }
+            }
+            return result;
+        }
+    }
+}
+
---
+++ Project01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Project01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberService numberService = new NumberService();
+            //Quest01
+            int inputNumOne = int.Parse(Console.ReadLine());
+            int resultAddNumbers = numberService.AddUpTheNumbers(inputNumOne);
+            Console.WriteLine(resultAddNumbers);
+            //Quest02
+            int inputNumTwo = int.Parse(Console.ReadLine());
+            numberService.WriteEvenNumbers(inputNumTwo);
+            //Quest03
+            int inputNumThree = int.Parse(Console.ReadLine());
+            bool numberIsEasy = numberService.CheckEasyNumbers(inputNumThree);
+            Console.WriteLine(numberIsEasy);
+            //Quest04
+            int inputNumFour = int.Parse(Console.ReadLine());
+            int resulAddEnteredNumbers = numberService.AddReadNumbers(inputNumFour);
+            Console.WriteLine(resulAddEnteredNumbers);
+            //Quest05
+            int inputNumFive = int.Parse(Console.ReadLine());
+            double resultAverageOfNumbers = numberService.AverageOfNumbers(inputNumFive);
+            Console.WriteLine(resultAverageOfNumbers);
+            //Quest06
+            int inputNumSix = int.Parse(Console.ReadLine());
+            int resultMinOfEnteredNumbers = numberService.FindMinNumbers(inputNumSix);
+            Console.WriteLine(resultMinOfEnteredNumbers);
+        }
+    }
+}
